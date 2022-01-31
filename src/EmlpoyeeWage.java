26aa1f83cb7b52691b583c49f3ec7c872f87a58d
prakashudikeri <pu010889@gmail.com>
2022-01-31 17:39:40 +0530

    Switch Case

--- src/EmlpoyeeWage.java
+++ src/EmlpoyeeWage.java
@@ -1,27 +1,32 @@
 
 public class EmlpoyeeWage {
 
-public static void main(String[] args) {
-		
-	int EMP_PART_TIME = 1;
-	int EMP_FULL_TIME = 2;
-	double empcheck = Math.floor(Math.random() * 10) %3;
-	int WAGE_PER_HOUR = 20;
-	int FULL_DAY_HOUR = 8;
-	int empWage = 0;
+	public static final int EMP_PART_TIME = 1;
+	public static final int EMP_FULL_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
+	
+	public static void main(String[] args) {
 			
-	if(empcheck == EMP_PART_TIME) {
-		System.out.println("Employee is Present & works Part Time");
-		empWage = (WAGE_PER_HOUR * FULL_DAY_HOUR)/2;
-		System.out.println("Employee Wage is: "+ empWage );
-	}
-	else if(empcheck == EMP_FULL_TIME) {
-	System.out.println("Employee is Present & works Full Time");
-	empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-	System.out.println("Employee Wage is: "+ empWage);	
-	}
-	else {
-	System.out.println("Employee is Absent");	
+		int empCheck = (int) Math.floor(Math.random() * 10) %3;
+		int empWage = 0;
+		int empHrs = 0;
+				
+		switch (empCheck) {
+		case EMP_PART_TIME:
+			 empHrs = 4;
+			 empWage = empHrs * WAGE_PER_HOUR;
+			 System.out.println("Employee is Part Time & Wage is: " +empWage);
+		break;
+		case EMP_FULL_TIME:
+			 empHrs = 8;
+			 empWage = empHrs * WAGE_PER_HOUR;
+			 System.out.println("Employee is Full Time & Wage is: " +empWage);
+		break;
+		default:
+			 empHrs = 0;
+			 empWage = empHrs * WAGE_PER_HOUR;
+			 System.out.println("Employee is Absent & Wage is: " +empWage);
 		}
+	
 	}
 }
