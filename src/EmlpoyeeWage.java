50f81fe6f171dec7531a39f96e3f94678742f3a9
prakashudikeri <pu010889@gmail.com>
2022-01-31 17:57:56 +0530

    Monthly Wage

--- src/EmlpoyeeWage.java
+++ src/EmlpoyeeWage.java
@@ -1,32 +1,33 @@
 
 public class EmlpoyeeWage {
 
-	public static final int EMP_PART_TIME = 1;
-	public static final int EMP_FULL_TIME = 2;
-	public static final int WAGE_PER_HOUR = 20;
-	
-	public static void main(String[] args) {
-			
-		int empCheck = (int) Math.floor(Math.random() * 10) %3;
-		int empWage = 0;
-		int empHrs = 0;
-				
-		switch (empCheck) {
-		case EMP_PART_TIME:
-			 empHrs = 4;
-			 empWage = empHrs * WAGE_PER_HOUR;
-			 System.out.println("Employee is Part Time & Wage is: " +empWage);
-		break;
-		case EMP_FULL_TIME:
-			 empHrs = 8;
-			 empWage = empHrs * WAGE_PER_HOUR;
-			 System.out.println("Employee is Full Time & Wage is: " +empWage);
-		break;
-		default:
-			 empHrs = 0;
-			 empWage = empHrs * WAGE_PER_HOUR;
-			 System.out.println("Employee is Absent & Wage is: " +empWage);
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 22;
+    public static final int MAX_HRS_IN_MONTH = 10;
+
+public static void main(String[] args) {
+    
+int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+    	
+	while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+totalWorkingDays++;
+int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+	switch (empCheck) {
+		case IS_PART_TIME:
+			empHrs = 4;
+            break;
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
+        default:
+            empHrs = 0;
 		}
-	
+		totalEmpHrs += empHrs;
+		System.out.println("DAy#:" + totalWorkingDays + "Emp Hr:" +empHrs);
 	}
+		int totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;
+		System.out.println("Total Emp wage:" + totalEmpWage);
+}
 }
