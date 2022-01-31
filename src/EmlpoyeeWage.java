4f8a6df950a3078a8c1f38500ea25259415d3220
prakashudikeri <pu010889@gmail.com>
2022-01-31 17:31:54 +0530

    EmployeePartTime

--- src/EmlpoyeeWage.java
+++ src/EmlpoyeeWage.java
@@ -3,16 +3,22 @@ public class EmlpoyeeWage {
 
 public static void main(String[] args) {
 		
-	int IS_EMP_PRESENT = 1;
-	double empcheck = (int) Math.floor(Math.random() * 10) %2;
+	int EMP_PART_TIME = 1;
+	int EMP_FULL_TIME = 2;
+	double empcheck = Math.floor(Math.random() * 10) %3;
 	int WAGE_PER_HOUR = 20;
 	int FULL_DAY_HOUR = 8;
 	int empWage = 0;
 			
-	if(empcheck == IS_EMP_PRESENT) {
-		System.out.println("Employee is Present");
-		empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-		System.out.println("Employee Wage: "+ empWage );
+	if(empcheck == EMP_PART_TIME) {
+		System.out.println("Employee is Present & works Part Time");
+		empWage = (WAGE_PER_HOUR * FULL_DAY_HOUR)/2;
+		System.out.println("Employee Wage is: "+ empWage );
+	}
+	else if(empcheck == EMP_FULL_TIME) {
+	System.out.println("Employee is Present & works Full Time");
+	empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+	System.out.println("Employee Wage is: "+ empWage);	
 	}
 	else {
 	System.out.println("Employee is Absent");	
